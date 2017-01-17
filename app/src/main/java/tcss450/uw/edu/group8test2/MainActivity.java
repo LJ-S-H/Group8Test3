a7957239f9e8ceb8cf29dac628f5100bdf93ab21
lovejithari <lovejitshari@gmail.com>
2017-01-17 08:22:52 -0800

    Secondary commit, checking if changes are in my branch

--- app/src/main/java/tcss450/uw/edu/group8test2/MainActivity.java
+++ app/src/main/java/tcss450/uw/edu/group8test2/MainActivity.java
@@ -3,6 +3,9 @@ package tcss450.uw.edu.group8test2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * Comment added to test commit.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
